aafbc2d36392f4587f3e34604b1371af38b1827a
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2025-11-21 10:13:33 +0100

    linting

--- src/ccmm_invenio/serializers/nma/datacite.py
+++ src/ccmm_invenio/serializers/nma/datacite.py
@@ -1,13 +1,27 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of ccmm-invenio (see https://github.com/NRP-CZ/ccmm-invenio).
+#
+# ccmm-invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""DataCite serializer for CCMM NMA records."""
+
+from __future__ import annotations
+
+from typing import Any
+
 from flask_resources import BaseListSchema, MarshmallowSerializer
 from flask_resources.serializers import JSONSerializer
 from invenio_rdm_records.resources.serializers.datacite import DataCite43Schema
 
 
-class CCMMNMADataCiteJSONSerializer_1_1_0(MarshmallowSerializer):
+class CCMMNMADataCiteJSONSerializer_1_1_0(MarshmallowSerializer):  # noqa: N801
     """Marshmallow based DataCite serializer for records."""
 
-    def __init__(self, **options):
-        """Constructor."""
+    def __init__(self, **options: Any):
+        """Create a new instance of the serializer."""
         super().__init__(
             format_serializer_cls=JSONSerializer,
             object_schema_cls=NMADataCiteSchema,
