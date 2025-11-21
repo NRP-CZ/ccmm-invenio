8707c9de97547f6cfe151bcbae1f3147dfe753ef
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2025-11-21 10:13:33 +0100

    linting

--- src/ccmm_invenio/serializers/__init__.py
+++ src/ccmm_invenio/serializers/__init__.py
@@ -1,3 +1,15 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of ccmm-invenio (see https://github.com/NRP-CZ/ccmm-invenio).
+#
+# ccmm-invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""Serializers for CCMM Invenio records."""
+
+from __future__ import annotations
+
 from .nma.datacite import CCMMNMADataCiteJSONSerializer_1_1_0
 from .production.datacite import CCMMProductionDataCiteJSONSerializer_1_1_0
 
