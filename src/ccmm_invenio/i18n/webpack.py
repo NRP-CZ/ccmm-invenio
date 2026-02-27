6f6662dfdbb53f2ebc6da121dcd534fe25bf92f6
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-02-27 12:50:48 +0100

    fixed linter issues

--- src/ccmm_invenio/i18n/webpack.py
+++ src/ccmm_invenio/i18n/webpack.py
@@ -1,5 +1,14 @@
-# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2026 CESNET z.s.p.o.
+#
+# This file is a part of ccmm-invenio (see https://github.com/NRP-CZ/ccmm-invenio).
+#
+# ccmm-invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
 """Webpack bundle for i18n support."""
+
+from __future__ import annotations
 
 from invenio_assets.webpack import WebpackThemeBundle
 
--- src/ccmm_invenio/ui/webpack.py
+++ src/ccmm_invenio/ui/webpack.py
@@ -1,4 +1,14 @@
+#
+# Copyright (c) 2026 CESNET z.s.p.o.
+#
+# This file is a part of ccmm-invenio (see https://github.com/NRP-CZ/ccmm-invenio).
+#
+# ccmm-invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
 """Invenio module for CCMM Invenio UI webpack bundle."""
+
+from __future__ import annotations
 
 from invenio_assets.webpack import WebpackThemeBundle
 
@@ -7,11 +17,11 @@ theme = WebpackThemeBundle(
     ".",
     default="semantic-ui",
     themes={
-        "semantic-ui": dict(
-            entry={},
-            dependencies={},
-            devDependencies={},
-            aliases={"@js/ccmm_invenio": "./js/ccmm_invenio"},
-        )
+        "semantic-ui": {
+            "entry": {},
+            "dependencies": {},
+            "devDependencies": {},
+            "aliases": {"@js/ccmm_invenio": "./js/ccmm_invenio"},
+        }
     },
 )
