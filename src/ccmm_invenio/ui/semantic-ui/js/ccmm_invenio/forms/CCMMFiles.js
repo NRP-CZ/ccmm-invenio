eda2fe5fefacb2da496511066d144f3bd4718640
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-04-10 08:38:59 +0200

    reordering and section customization

---
+++ src/ccmm_invenio/ui/semantic-ui/js/ccmm_invenio/forms/CCMMFiles.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { buildUID } from "react-searchkit";
+import Overridable from "react-overridable";
+import { i18next } from "@translations/ccmm_invenio";
+import { UppyUploader } from "@js/invenio_rdm_records";
+
+export const CCMMFiles = {
+  key: "files",
+  label: i18next.t("Upload files"),
+  component: (tabConfig) => {
+    const { record, formConfig } = tabConfig;
+    const { filesLocked } = formConfig.config;
+    const { overridableIdPrefix } = formConfig;
+    return (
+      <Overridable
+        id={buildUID(overridableIdPrefix, "Files")}
+        {...tabConfig}
+      >
+        <UppyUploader
+          isDraftRecord={!record.is_published}
+          config={formConfig}
+          quota={formConfig.quota}
+          decimalSizeDisplay={formConfig.decimal_size_display}
+          allowEmptyFiles={formConfig.allow_empty_files}
+          fileUploadConcurrency={formConfig.file_upload_concurrency}
+          showMetadataOnlyToggle={false}
+          filesLocked={filesLocked}
+        />
+      </Overridable>
+    );
+  },
+  includesPaths: ["files.enabled"],
+};
